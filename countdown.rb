102ab48b36bc33d7eba64fcf58ee9630dc440694
Drew Price <drewprice@users.noreply.github.com>
2015-07-30 13:15:06 -0400

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+# write your code here
 
-def countdown
+def countdown(seconds, sleepy = false)
+  seconds.downto(1) do |s|
+    sleep 1 if sleepy
+    puts "#{s} SECOND(S)!"
+  end
+
+  'HAPPY NEW YEAR!'
+end
+
+def countdown_with_sleep(seconds)
+  countdown(seconds, true)
 end
